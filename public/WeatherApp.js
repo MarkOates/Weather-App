13adc187a3a90bd241e9e6799c51b43d2410f8a6
Mark Oates <euph_boy@hotmail.com>
2014-11-20 02:04:19 -0500

    Plotted city coordinates (in test data)

--- public/WeatherApp.js
+++ public/WeatherApp.js
@@ -1,17 +1,15 @@
 function WeatherApp($scope) {
 	$scope.cities = [
-		// these need to be plotted, on a 0-100% positioning on the map
-		// toronto is given as an example
 		{name:"Toronto",temp:4,population:6000,x:68,y:93},
-		{name:"Ottawa",temp:6,population:6000,x:50,y:50},
-		{name:"Montreal",temp:8,population:6000,x:50,y:50},
-		{name:"Quebec",temp:3,population:6000,x:50,y:50},
-		{name:"Fredericton",temp:2,population:6000,x:50,y:50},
-		{name:"Halifax",temp:2,population:6000,x:50,y:50},
-		{name:"Charlottetown",temp:7,population:6000,x:50,y:50},
-		{name:"St. Johns",temp:7,population:6000,x:50,y:50},
-		{name:"Thunder Bay",temp:9,population:6000,x:50,y:50},
-		{name:"Winnipeg",temp:9,population:6000,x:50,y:50}
+		{name:"Ottawa",temp:6,population:6000,x:73,y:87},
+		{name:"Montreal",temp:8,population:6000,x:76,y:85},
+		{name:"Quebec",temp:3,population:6000,x:78,y:81},
+		{name:"Fredericton",temp:2,population:6000,x:85,y:79},
+		{name:"Halifax",temp:2,population:6000,x:90,y:80},
+		{name:"Charlottetown",temp:7,population:6000,x:89,y:75},
+		{name:"St. Johns",temp:7,population:6000,x:100,y:63},
+		{name:"Thunder Bay",temp:9,population:6000,x:52,y:84},
+		{name:"Winnipeg",temp:9,population:6000,x:42,y:81}
 	];
 	$scope.show_map = false;
 }
