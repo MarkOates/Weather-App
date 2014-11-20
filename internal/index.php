0dd16ac1792a4f94c4aacfd27e1e148fbd78a282
Mark Oates <euph_boy@hotmail.com>
2014-11-20 00:35:09 -0500

    added map view, toggle view buttons

--- internal/index.php
+++ internal/index.php
@@ -12,13 +12,25 @@
 	<img id="maple_flag" src="public/canadian_maple.png">
 	<h1>Top 10<br>Coldest Canadian Cities</h1>
 	<div ng-app="" ng-controller="WeatherApp">
-		<table>
+		<ul id="controls">
+			<li><button type="button" ng-click="show_map=false;">Show List</button></li>
+			<li><button type="button" ng-click="show_map=true;">Show Map</button></li>
+		</ul>
+		<br clear="all">
+		<table ng-hide="show_map">
 			<tr ng-repeat="city in cities | orderBy:'temp'">
 				<td>{{ $index + 1 }}</td>
 				<td>{{ city.name }}</td>
 				<td>{{ city.temp }}&deg; C</td>
 			</tr>
 		</table>
+		<div class="canada_map" ng-show="show_map">
+			<img src="public/canada_map_large-01.png" style="width: 100%;">
+			<div class="city_marker" ng-style="{'left':'{{city.x}}%','top':'{{city.y}}%'}" ng-repeat="city in cities">
+				<img class="marker" src="public/marker_point-01.png">
+				<span>{{ city.name + " (" + city.temp + "&deg;C)" }}</span>
+			</div>
+		</div>
 		<div id="update_data">Page was last updated {{ update_string }}</div>
 	</div>
 
--- public/WeatherApp.js
+++ public/WeatherApp.js
@@ -1,14 +1,17 @@
 function WeatherApp($scope) {
 	$scope.cities = [
-		{name:"Houston",temp:4,population:6000},
-		{name:"Charlston",temp:6,population:6000},
-		{name:"Frankfurt",temp:8,population:6000},
-		{name:"Toronto",temp:3,population:6000},
-		{name:"Niagra Falls",temp:2,population:6000},
-		{name:"Yellowknife",temp:2,population:6000},
-		{name:"Whitehorse",temp:7,population:6000},
-		{name:"St. Johns",temp:7,population:6000},
-		{name:"Halifax",temp:9,population:6000},
-		{name:"Jasper",temp:9,population:6000}
+		// these need to be plotted, on a 0-100% positioning on the map
+		// toronto is given as an example
+		{name:"Toronto",temp:4,population:6000,x:68,y:93},
+		{name:"Ottawa",temp:6,population:6000,x:50,y:50},
+		{name:"Montreal",temp:8,population:6000,x:50,y:50},
+		{name:"Quebec",temp:3,population:6000,x:50,y:50},
+		{name:"Fredericton",temp:2,population:6000,x:50,y:50},
+		{name:"Halifax",temp:2,population:6000,x:50,y:50},
+		{name:"Charlottetown",temp:7,population:6000,x:50,y:50},
+		{name:"St. Johns",temp:7,population:6000,x:50,y:50},
+		{name:"Thunder Bay",temp:9,population:6000,x:50,y:50},
+		{name:"Winnipeg",temp:9,population:6000,x:50,y:50}
 	];
+	$scope.show_map = false;
 }
