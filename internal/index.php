bb8a0db2cfee3b0dee543d31709cf8bbda9e4d06
Mark Oates <euph_boy@hotmail.com>
2014-11-19 23:28:31 -0500

    Slight css changes (added a maple leaf)

--- internal/index.php
+++ internal/index.php
@@ -8,16 +8,20 @@
 </head>
 
 <body>
+
+	<img id="maple_flag" src="public/canadian_maple.png">
 	<h1>Top 10<br>Coldest Canadian Cities</h1>
 	<div ng-app="" ng-controller="WeatherApp">
 		<table>
-			<tr ng-repeat="city in cities | orderBy:temp">
+			<tr ng-repeat="city in cities | orderBy:'temp'">
 				<td>{{ $index + 1 }}</td>
 				<td>{{ city.name }}</td>
 				<td>{{ city.temp }}&deg; C</td>
 			</tr>
 		</table>
+		<div id="update_data">Page was last updated {{ update_string }}</div>
 	</div>
+
 </body>
 
 </html>
