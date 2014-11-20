f291998ed391628e87a8e9a11d7487e2d55bbcdd
Mark Oates <euph_boy@hotmail.com>
2014-11-20 04:42:58 -0500

    Added scraper.

---
+++ internal/tests/scraper_test.php
@@ -0,0 +1,57 @@
+<?php
+
+
+
+
+//
+// grab the contents from the source
+//
+
+$location = "internal/tests/scraper_test_data.htm"; // a locally stored copy of...
+//$location = "https://weather.gc.ca/canada_e.html"; // <-- this
+
+$content_to_scrape = file_get_contents($location);
+
+
+
+//
+// extract just the tbody
+//
+
+$table_start_pos = strpos($content_to_scrape, "<tbody");
+$table_end_pos = strpos($content_to_scrape, "</tbody>") + strlen("</tbody>");
+
+$table_of_data = substr($content_to_scrape, $table_start_pos, $table_end_pos - $table_start_pos);
+
+
+
+//
+// Now we have just the tbody with data. Scrape my monkeys! ScRAPE!
+//
+
+$matches = array();
+$scraper_pattern = '/html\'>(.+)<\/a>.+<td>(.+)&deg;C<\/td><\/tr/';
+$results = null;
+
+if (preg_match_all($scraper_pattern, $table_of_data, $matches))
+{
+	// patterns captured successfully
+	$results = array();
+	for ($i=0; $i<sizeof($matches[0]); $i++)
+	{
+		$results[$i]["city"] = $matches[1][$i];
+		$results[$i]["temp"] = $matches[2][$i];
+	}
+}
+else
+{
+	echo "there was an error matching the patterns.";
+}
+
+
+
+//
+// Output the results in json format
+//
+
+echo json_encode($results);
